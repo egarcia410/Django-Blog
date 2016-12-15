f96a997f3d4ba761c9001032605b6df1b10e390b
Emmanuel Garcia <egarcia410@gmail.com>
2016-12-14 20:49:23 -0600

    Create: file images

--- venv/src/posts/views.py
+++ venv/src/posts/views.py
@@ -1,12 +1,14 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
 
+
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -18,6 +20,7 @@ def post_create(request):
 	}
 	return render(request, "post_form.html", context)
 
+
 def post_detail(request, id=None):
 	instance = get_object_or_404(Post, id=id)
 	context = {
@@ -26,18 +29,31 @@ def post_detail(request, id=None):
 	}
 	return render(request, "post_detail.html", context)
 
+
 def post_list(request):
-	queryset = Post.objects.all()
-	context = {
-		"object_list": queryset,
-		"title": "List"
-	}
-	return render(request, "post_list.html", context)
+    queryset = Post.objects.all() #.order_by('-timestamp')
+    paginator = Paginator(queryset, 2) # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "List",
+        'page_request_var': page_request_var
+    }
+    return render(request, "post_list.html", context)
 
 
 def post_update(request, id=None):
 	instance = get_object_or_404(Post, id=id)
-	form = PostForm(request.POST or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -50,7 +66,6 @@ def post_update(request, id=None):
 		"form":form,
 	}
 	return render(request, "post_form.html", context)
-
 
 
 def post_delete(request, id=None):
